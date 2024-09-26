f8239291fabc651ae5a5bf15df34c2d5d559629b
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2024-09-26 18:39:00 +0500

    app store link have added

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -128,7 +128,7 @@ export default {
                 <a target="_blank" href='https://play.google.com/store/apps/details?id=com.raqobat.fairtech_mobile&pcampaignid=web_share&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img width="128"  alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
               </b-col>
               <b-col>
-                <a class="disabled-link" target="_blank" title="FAIRTECH" href="https://testflight.apple.com/join/F59RPltZ"><img width="128" alt="Download on the App Store Badge" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/128px-Download_on_the_App_Store_Badge.svg.png"></a>
+                <a target="_blank" title="FAIRTECH" href="https://apps.apple.com/uz/app/fairtech/id6505064805"><img width="128" alt="Download on the App Store Badge" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/128px-Download_on_the_App_Store_Badge.svg.png"></a>
               </b-col>
             </b-row>
           </b-modal>
